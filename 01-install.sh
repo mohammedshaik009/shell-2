72567942893d1c32e048f02418296181e6a21a24
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-30 11:13:44 +0530

    shell script started

---
+++ 01-install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+AMI_ID="0220d79f3f480ecf5"
+ZONE_ID="Z083970011QGFE38SJQH9"
+DOMAIN_NAME=mohammed.world
+
+echo "installing instance:$instance"
+INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
+        )
+echo "INSTANCE_ID:$INSTANCE_ID"
+    if [ $instance == frontend ]; then
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text
+        )
+    R53_RECORD=$DOMAIN_NAME
+    else
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text
+        )
+    R53_RECORD=$instance.$DOMAIN_NAME
+    fi
+    ### updating R53_RECORD ###
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+            {
+                "Comment": "Update A record to new IP",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": "'$R53_RECORD'",
+                            "Type": "A",
+                            "TTL": 1,
+                            "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    '
+done
+
+        
