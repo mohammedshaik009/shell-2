71da573b4cce4236fa5279aa67021b1b6bafc471
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-30 17:14:00 +0530

    shell script started

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -33,11 +33,11 @@ VALIDATE $? "adding mongo.repo"
 dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "installing MongoDB"
 
-systemctl enable --now mongod
+systemctl enable --now mongod &>> $LOGS_FILE
 VALIDATE $? "starting and enabling MongoDB"
 
-sed -i '/s/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i -e '/s/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
-systemctl restart mongod
+systemctl restart mongod   &>> $LOGS_FILE
 VALIDATE $? "Restarting MongoDB"
