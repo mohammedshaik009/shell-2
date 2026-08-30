370bb60a595af43eae73bf606415848f9eadc916
mohammedshaik009 <mohammedshaik009@gmail.com>
2026-08-30 19:13:44 +0530

    shell script started

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -34,7 +34,7 @@ dnf install nodejs -y   &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
 
 id roboshop  &>> $LOGS_FILE
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "setting up system user"
 else
